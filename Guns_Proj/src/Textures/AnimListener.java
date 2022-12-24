7c301d7c91c2b9dae737797904b5c1b812a13038
Salahsaed0 <salahsaed599@gmail.com>
2022-12-24 23:43:22 +0200

    Created link between pages and code(soldiers)

---
+++ Guns_Proj/src/Textures/AnimListener.java
@@ -0,0 +1,13 @@
+
+package Textures;
+
+import java.awt.event.KeyListener;
+import javax.media.opengl.GLEventListener;
+
+
+
+public abstract class AnimListener implements GLEventListener, KeyListener {
+ 
+    protected String assetsFolderName = "Assets";
+    
+}
